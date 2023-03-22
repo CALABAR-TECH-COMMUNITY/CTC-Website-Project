fca2a05f637c1d1fac351521d33944fc50bece06
queencobham <queencobham@gmail.com>
2023-03-22 07:00:50 -0700

    About-page

--- src/navigation/AppRouter.tsx
+++ src/navigation/AppRouter.tsx
@@ -2,6 +2,7 @@ import HomePage from "pages/HomePage";
 import PageNotFound from "pages/PageNotFound";
 import ProjectDetailsPage from "pages/projects/ProjectDetailsPage";
 import ProjectsPage from "pages/projects/ProjectsPage";
+import AboutPage from "pages/about/AboutPage";
 import { Route, Routes } from "react-router-dom";
 
 import routes from "./routes";
@@ -17,6 +18,7 @@ const AppRouter = (): JSX.Element => {
         path={routes.PROJECT_DETAILS_PAGE}
         element={<ProjectDetailsPage />}
       />
+      <Route path={routes.ABOUT_PAGE} element={<AboutPage />} />
 
       {/* 404 PAGE: NOTE THIS MUST BE THE LAST ROUTE */}
       <Route path="*" element={<PageNotFound />} />
--- src/navigation/routes.ts
+++ src/navigation/routes.ts
@@ -2,6 +2,7 @@ export default Object.freeze({
   HOME_PAGE: "/",
   PROJECTS_PAGE: "/projects",
   PROJECT_DETAILS_PAGE: "/project-details",
+  ABOUT_PAGE: "/about",
 
   // EXTERNAL SOCIAL ROUTES
 });
