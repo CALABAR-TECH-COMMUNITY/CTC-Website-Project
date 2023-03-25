645190a84112118a49f3c0ca8d34cd5a62758861
David Edu <daveeedu@gmail.com>
2023-03-25 14:43:15 +0100

    add footer

--- src/utils/icons.utils.ts
+++ src/utils/icons.utils.ts
@@ -1,4 +1,17 @@
 import Logo from "../assets/images/Logo.svg";
+import Twitter from "../assets/images/twitter.svg";
+import Whatsapp from "../assets/images/whatsapp.svg";
+import Slack from "../assets/images/slack.svg";
+import LogoFooter from "../assets/images/logoFooter.svg";
+import ArrowUp from "../assets/images/arrowUp.svg";
 
 
 export const ILogo = Logo;
+export const ITwitter = Twitter;
+export const IWhatsapp= Whatsapp;
+export const ISlack= Slack;
+export const ILogoFooter= LogoFooter;
+export const IArrowUp= ArrowUp;
+
+
+
