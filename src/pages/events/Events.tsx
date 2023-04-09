fd4f303b7889687b37ca8cb8ae7f8cac4014330f
DandyJoshua14 <dandyjoshua14@gmail.com>
2023-04-09 17:35:44 +0100

    mod3

--- src/pages/events/Events.tsx
+++ src/pages/events/Events.tsx
@@ -65,66 +65,105 @@ function Events() {
   ];
   return (
     <HomeLayout>
-    <main className="font-comfort">
-      <header className="h-32 md:h-36 lg:h-40 bg-[url(assets/images/header.svg)] bg-cover bg-center bg-no-repeat overflow-hidden flex items-center ">
-        <h1 className="text-6xl font-weight-700 text-left font-comfort sm:text-3xl md:text-5xl ml-11 md:ml-12 lg:ml-14 ">
-          Events
-        </h1>
-      </header>
+      <main className="font-comfort">
+        <header className="h-32 md:h-36 lg:h-40 bg-[url(assets/images/header.svg)] bg-cover bg-center bg-no-repeat overflow-hidden flex items-center ">
+          <h1 className="text-6xl font-weight-700 text-left font-comfort sm:text-3xl md:text-5xl ml-11 md:ml-12 lg:ml-14 ">
+            Events
+          </h1>
+        </header>
 
-      <h2 className="text-center text-size-48 leading-lineh-54 tracking-custom1 font-weight-700 font-comfort h-[54px] top-[110px] py-20 text-darkbtn">
-        Upcoming Events
-      </h2>
+        <h2 className="text-center text-size-48 leading-lineh-54 tracking-custom1 font-weight-700 font-comfort h-[54px] top-[110px] py-20 text-darkbtn">
+          Upcoming Events
+        </h2>
 
-      {/* Grids */}
-      <section className="grid grid-cols-1 pt-20 pb-14 md:grid-cols-2 overflow-hidden lg:grid-cols-3 responsive-padx-container gap-10 place-content-center ml-11">
-        {gridDetails.map((details, index) => {
-          return <EventCard key={index+1} {...details} />;
-        })}
-        <br/>
-        <br/>
-      </section>
-      {/* End of Grids*/}
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <section className="w-full bg-[#1D1D21] p-0 overflow-hidden relative">
-        <p className="font-extrabold text-white text-center font-comforta text-size-48 pt-1">Past Events</p>
-        <div className="px-[50px] py-9 lg:px-[500px] md:px-[200px] flex">
-        <br/>
-        <img src={flyer1} className="w-[150px] h-[100px] rounded-xl border-[5px] border-darkbtn sm:w-[50px]h-[50px]" alt="flyer 1"/>
-        <p className="pl-[20px] text-white font-extrabold font-poppins">Feb 23, 2023 - Virtual (Google meet)<br/>
-        <h3 className="text-xl font-comforta">Introduction to tech</h3>
-        <div className="flex flex-row flex-wrap font-extralight whitespace-nowrap overflow-hidden overflow-ellipsis">
-        <p className="border rounded-full p-2">Career Talks</p>&nbsp;<p className="border rounded-full p-2">Product Manaegment</p>&nbsp;<p className="border rounded-full p-2">Mobile App Dev.</p><p className="border rounded-full p-2">Software Engineering</p><br/><p></p><br/>
-        </div>
-        <p className="font-light flex-wrap">Lorem ipsum dolor sit amet consectetur. Felis eu feugiat tincidunt lacus vitae sit. Mollis adipiscing orci ut donec porttitor nulla cursus at. Venenatis cras ullamcorper adipiscing risus leo lobortis non. In purus laoreet at imperdiet tempus auctor magna. Turpis mi purus vivamus diam tortor mi lacinia integer at. Massa tincidunt sit cum mattis habitasse duis risus.</p>
-        </p>
-      </div>
-      <br/>
-      <div className="px-[50px] lg:px-[200px] xl:px-[500px] py-9 flex">
-        <br/>
-        <img src={flyer2} className="w-[150px] h-[100px] rounded-xl border-[5px] border-darkbtn sm:w-[50px]h-[50px]" alt="flyer 1"/>
-        <p className="pl-[20px] text-white font-extrabold font-poppins">Feb 23, 2023 - Virtual (Google meet)<br/>
-        <h3 className="text-xl font-comforta">Introduction to tech</h3>
-        <div className="flex flex-row flex-wrap font-extralight whitespace-nowrap overflow-hidden overflow-ellipsis">
-        <p className="border rounded-full p-2">Career Talks</p>&nbsp;<p className="border rounded-full p-2">Product Manaegment</p>&nbsp;<p className="border rounded-full p-2">Mobile App Dev.</p><p className="border rounded-full p-2">Software Engineering</p><br/><p></p><br/>
-        </div>
-        <p className="font-light flex-wrap">Lorem ipsum dolor sit amet consectetur. Felis eu feugiat tincidunt lacus vitae sit. Mollis adipiscing orci ut donec porttitor nulla cursus at. Venenatis cras ullamcorper adipiscing risus leo lobortis non. In purus laoreet at imperdiet tempus auctor magna. Turpis mi purus vivamus diam tortor mi lacinia integer at. Massa tincidunt sit cum mattis habitasse duis risus.</p>
-        </p>
-      </div>
-      <div className="relative h-1/5 p-0">
-      <img
-        className="absolute bottom-0 right-0"
-        src={gradient}
-        alt="Gradient"
-      />
-    </div>
-      </section>
-      <br/>
-      <br/>
-    </main>
+        {/* Grids */}
+        <section className="grid grid-cols-1 pt-20 pb-14 md:grid-cols-2 overflow-hidden lg:grid-cols-3 responsive-padx-container gap-10 place-content-center ml-11">
+          {gridDetails.map((details, index) => {
+            return <EventCard key={index + 1} {...details} />;
+          })}
+          <br />
+          <br />
+        </section>
+        {/* End of Grids*/}
+        <br />
+        <br />
+        <br />
+        <br />
+        <section className="w-full bg-[#1D1D21] p-0 overflow-hidden relative">
+          <p className="font-extrabold text-white text-center font-comforta text-size-48 pt-1">
+            Past Events
+          </p>
+          <div className=" gap-10 sm:gap-0 text-center sm:text-start responsive-padx-container mx-auto max-w-[66rem] py-9 flex flex-wrap sm:flex-nowrap">
+            <br />
+            <img
+              src={flyer1}
+              className="mx-auto w-[150px] h-[100px] rounded-xl border-[5px] border-darkbtn sm:w-[50px]h-[50px]"
+              alt="flyer 1"
+            />
+            <p className="pl-[20px] text-white font-extrabold font-poppins">
+              Feb 23, 2023 - Virtual (Google meet)
+              <br />
+              <h3 className="text-xl font-comforta">Introduction to tech</h3>
+              <div className="flex flex-row flex-wrap font-extralight whitespace-nowrap overflow-hidden overflow-ellipsis">
+                <p className="border rounded-full p-2">Career Talks</p>
+                <p className="border rounded-full p-2">Product Manaegment</p>
+                &nbsp;<p className="border rounded-full p-2">Mobile App Dev.</p>
+                <p className="border rounded-full p-2">Software Engineering</p>
+                <br />
+                <br />
+              </div>
+              <p className="font-light flex-wrap">
+                Lorem ipsum dolor sit amet consectetur. Felis eu feugiat
+                tincidunt lacus vitae sit. Mollis adipiscing orci ut donec
+                porttitor nulla cursus at. Venenatis cras ullamcorper adipiscing
+                risus leo lobortis non. In purus laoreet at imperdiet tempus
+                auctor magna. Turpis mi purus vivamus diam tortor mi lacinia
+                integer at. Massa tincidunt sit cum mattis habitasse duis risus.
+              </p>
+            </p>
+          </div>
+          <br />
+          <div className="px-[50px] responsive-padx-container py-9 flex">
+            <br />
+            <img
+              src={flyer2}
+              className="w-[150px] h-[100px] rounded-xl border-[5px] border-darkbtn sm:w-[50px]h-[50px]"
+              alt="flyer 1"
+            />
+            <p className="pl-[20px] text-white font-extrabold font-poppins">
+              Feb 23, 2023 - Virtual (Google meet)
+              <br />
+              <h3 className="text-xl font-comforta">Introduction to tech</h3>
+              <div className="flex flex-row flex-wrap font-extralight whitespace-nowrap overflow-hidden overflow-ellipsis">
+                <p className="border rounded-full p-2">Career Talks</p>&nbsp;
+                <p className="border rounded-full p-2">Product Manaegment</p>
+                &nbsp;<p className="border rounded-full p-2">Mobile App Dev.</p>
+                <p className="border rounded-full p-2">Software Engineering</p>
+                <br />
+                <p></p>
+                <br />
+              </div>
+              <p className="font-light flex-wrap">
+                Lorem ipsum dolor sit amet consectetur. Felis eu feugiat
+                tincidunt lacus vitae sit. Mollis adipiscing orci ut donec
+                porttitor nulla cursus at. Venenatis cras ullamcorper adipiscing
+                risus leo lobortis non. In purus laoreet at imperdiet tempus
+                auctor magna. Turpis mi purus vivamus diam tortor mi lacinia
+                integer at. Massa tincidunt sit cum mattis habitasse duis risus.
+              </p>
+            </p>
+          </div>
+          <div className="relative h-1/5 p-0">
+            <img
+              className="absolute bottom-0 right-0"
+              src={gradient}
+              alt="Gradient"
+            />
+          </div>
+        </section>
+        <br />
+        <br />
+      </main>
     </HomeLayout>
   );
 }
