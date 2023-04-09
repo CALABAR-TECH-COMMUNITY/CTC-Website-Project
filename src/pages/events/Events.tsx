603ef93259393b123b9bfd0182c33b84c4660add
DandyJoshua14 <dandyjoshua14@gmail.com>
2023-04-09 16:24:36 +0100

    Added gradient to section and improved responsiveness

--- src/pages/events/Events.tsx
+++ src/pages/events/Events.tsx
@@ -2,6 +2,7 @@ import flyer1 from "assets/images/flyer1.png";
 import flyer2 from "assets/images/flyer2.png";
 import flyer3 from "assets/images/flyer3.png";
 import flyer4 from "assets/images/flyer4.png";
+import gradient from "assets/images/gradient.png";
 import HomeLayout from "layout/HomeLayout";
 import EventCard from "./components/EventCard";
 
@@ -88,9 +89,9 @@ function Events() {
       <br/>
       <br/>
       <br/>
-      <section className="w-full bg-[#1D1D21] p-[10px] overflow-hidden">
+      <section className="w-full bg-[#1D1D21] p-0 overflow-hidden relative">
         <p className="font-extrabold text-white text-center font-comforta text-size-48">Past Events</p>
-        <div className="px-[50px] flex">
+        <div className="px-[50px] py-9 lg:px-[500px] md:px-[20px] flex">
         <br/>
         <img src={flyer1} className="w-[150px] h-[100px] rounded-xl border-[5px] border-darkbtn sm:w-[50px]h-[50px]" alt="flyer 1"/>
         <p className="pl-[20px] text-white font-extrabold font-poppins">Feb 23, 2023 - Virtual (Google meet)<br/>
@@ -102,7 +103,7 @@ function Events() {
         </p>
       </div>
       <br/>
-      <div className="px-[50px] flex">
+      <div className="px-[50px] lg:px-[500px] py-9 flex">
         <br/>
         <img src={flyer2} className="w-[150px] h-[100px] rounded-xl border-[5px] border-darkbtn sm:w-[50px]h-[50px]" alt="flyer 1"/>
         <p className="pl-[20px] text-white font-extrabold font-poppins">Feb 23, 2023 - Virtual (Google meet)<br/>
@@ -113,17 +114,13 @@ function Events() {
         <p className="font-light flex-wrap">Lorem ipsum dolor sit amet consectetur. Felis eu feugiat tincidunt lacus vitae sit. Mollis adipiscing orci ut donec porttitor nulla cursus at. Venenatis cras ullamcorper adipiscing risus leo lobortis non. In purus laoreet at imperdiet tempus auctor magna. Turpis mi purus vivamus diam tortor mi lacinia integer at. Massa tincidunt sit cum mattis habitasse duis risus.</p>
         </p>
       </div>
-      {/* <div className="px-[500px] pt-[200px] flex">
-        <br/>
-        <img src={flyer2} className="w-[200px] h-[150px] rounded-xl border-[5px] border-darkbtn" alt="flyer 1"/>
-        <p className="pl-[20px] text-white font-extrabold font-poppins">Feb 23, 2023 - Virtual (Google meet)<br/>
-        <h3 className="text-xl font-comforta">Introduction to tech</h3>
-        <div className="flex flex-row justify-items-center font-extralight whitespace-nowrap overflow-hidden overflow-ellipsis">
-        <p className="border rounded-full p-2">Career Talks</p><p className="border rounded-full p-2">Product Manaegment</p><p className="border rounded-full p-2">Mobile App Dev.</p><p className="border rounded-full p-2">Software Engineering</p><br/><p></p><br/>
-        </div>
-        <p className="font-light">Lorem ipsum dolor sit amet consectetur. Felis eu feugiat tincidunt lacus vitae sit. Mollis adipiscing orci ut donec porttitor nulla cursus at. Venenatis cras ullamcorper adipiscing risus leo lobortis non. In purus laoreet at imperdiet tempus auctor magna. Turpis mi purus vivamus diam tortor mi lacinia integer at. Massa tincidunt sit cum mattis habitasse duis risus.</p>
-        </p>
-      </div> */}
+      <div className="relative h-1/5 p-0">
+      <img
+        className="absolute bottom-0 right-0"
+        src={gradient}
+        alt="Gradient"
+      />
+    </div>
       </section>
       <br/>
       <br/>
