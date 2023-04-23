af5f84877eb8bbfd31102aea300c9fbf575fcba4
sirsuccess <kanuamani@gmail.com>
2023-04-23 08:11:44 +0100

    fix handbuger

--- src/pages/home/sections/HomeHeader.tsx
+++ src/pages/home/sections/HomeHeader.tsx
@@ -1,12 +1,22 @@
 import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
-import { GiHamburgerMenu } from "react-icons/gi";
+import {
+  AiOutlineMenu,
+  AiOutlineClose,
+} from "react-icons/ai";
 
 import AppLogo from "assets/images/Logo.svg";
 import { tabs } from "layout/HomeLayout";
 
 const HomeHeader = (): JSX.Element => {
   const location = useLocation();
+  const [showMenu, setShowMenu] = useState(false);
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabClick = (index: number) => {
+    setActiveTab(index);
+  };
 
   return (
     <header className="h-20 w-full  text-white flex justify-between items-center font-reem responsive-padx-container">
@@ -27,9 +37,43 @@ const HomeHeader = (): JSX.Element => {
           );
         })}
       </nav>
+      {showMenu && (
+        <div className="md:hidden  pb-4 w-[100%] flex gap-4">
+          {tabs.map((tab) => (
+            <Link
+              key={tab.index}
+              to={tab.link}
+              onClick={() => handleTabClick(tab.index)}
+              className={`block mt-4 md:inline-block md:mt-0 md:ml-6 ${
+                activeTab === tab.index
+                  ? "font-[700] text-[12px]"
+                  : "font-normal text-[12px]"
+              }`}
+            >
+              {tab.name}
+            </Link>
+          ))}
+        </div>
+      )}
 
       <FiSearch className="hidden sm:block" />
-      <GiHamburgerMenu className="sm:hidden" />
+      {/* <GiHamburgerMenu
+        className="sm:hidden"
+        onClick={() => setShowMenu(!showMenu)}
+      /> */}
+         <div className="md:hidden">
+              <button
+                onClick={() => setShowMenu(!showMenu)}
+                type="button"
+                className="text-gray-500 hover:text-white focus:outline-none focus:text-white"
+              >
+                {showMenu ? (
+                  <AiOutlineClose className="w-[30px] h-[30px]" />
+                ) : (
+                  <AiOutlineMenu className="w-[30px] h-[30px]" />
+                )}
+              </button>
+            </div>
     </header>
   );
 };
